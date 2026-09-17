41f8a94388186f22b9672887c6937be5f72d3150
polibiopipe <139810702+polibiopipe@users.noreply.github.com>
2026-09-17 16:28:32 -0700

    Add standalone seminar entry in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { SeminarStandaloneApp } from "./seminar/SeminarStandaloneApp.jsx";
 import "./styles.css";
 import "./premium.css";
 import "./visual-refresh.css";
 
+const RootApplication = globalThis.location?.pathname === "/ruta-seminario"
+  ? SeminarStandaloneApp
+  : App;
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RootApplication />
   </React.StrictMode>
 );
